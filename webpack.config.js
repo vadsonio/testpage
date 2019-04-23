46234b2d9b76ebbd81194f49e119e3a0565bcc72
vadsonio <benz-60050@yandex.ru>
2019-04-23 16:08:40 +0300

    added supported files

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin'); 
+ 
+module.exports = {
+ mode: 'none',
+ entry: './src/main.js',
+ output: {
+   path: path.resolve(__dirname, './dist'),
+   publicPath: '/dist/',
+   filename: 'build.js'
+ },
+ module: {
+   rules: [
+     {
+       test: /\.vue$/,
+       loader: 'vue-loader'
+     }, {
+      test: /\.css$/,
+      use: [
+        'vue-style-loader',
+        'css-loader'
+      ]
+    }
+   ]
+ },
+ plugins: [
+    new VueLoaderPlugin()
+   ]
+}
